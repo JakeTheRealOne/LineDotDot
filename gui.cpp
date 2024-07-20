128f115ad937816802cc1365b3cac2ecf0426fe8
JakeTheRealOne <jakelevrai@outlook.be>
2024-07-20 23:02:43 +0200

    Recreation of the bottom bar

--- gui.cpp
+++ gui.cpp
@@ -21,10 +21,23 @@
 # include <QSize>
 # include <QString>
 # include <QTextEdit>
+# include <QLabel>
 # include <string>
 # include "source/convert.hpp"
-# include <iostream>
 using namespace std;
+
+class inputBox : public QWidget {
+    // Here we have the box where characters are written for translation (base: english)
+    // += SOS button, += settings button
+public:
+    inputBox () {
+
+    }
+private:
+    QWidget main, buttonsBar, settingsBar;
+    QTextEdit textBox;
+    QPushButton settingsButton = QPushButton("settings"), emergencyButton = QPushButton("emergency");
+};
 
 class GUI : public QWidget {
 public:
@@ -37,22 +50,26 @@ public:
     };
 
 private:
-    // #### Attributes:
-    QWidget bottomBar, topBar, translateBox;
+    // #### Attributes: ####
+    QWidget bottomBar, topBar, translateBox, switchBox;
     QVBoxLayout mainLayout, translateLayout;
-    QHBoxLayout bottomLayout, topLayout;
-    QPushButton closeButton = QPushButton(""), encyclopediaButton = QPushButton(""), radioButton = QPushButton("󰜟"), settingsButton = QPushButton(""), encodingButton = QPushButton("Write morse"), decodingButton = QPushButton("Read morse"), conversionButton = QPushButton("Convert");
-    QTextEdit englishTextBox, morseTextBox;
+    QHBoxLayout bottomLayout, topLayout, sbLayout;
+    QPushButton closeButton = QPushButton(""), encyclopediaButton = QPushButton(""), radioButton = QPushButton("󰜟"), settingsButton = QPushButton(""),textButton = QPushButton(""), fileButton = QPushButton("󰈙"), materialButton = QPushButton("󰳽"), flashButton = QPushButton(""), swapButton = QPushButton("󰯎");
+    QTextEdit inputTextBox, outputTextBox;
+    QLabel warningLabel = QLabel("File mode is recommended for processing large amounts of data.");
 
-    // 
+    bool inputIsMorse = false;
+    char currentMode = 0; // 0: text mode, 1: file mode, 2: material mode, 3: flash mode
+
+    // #### Building methods: ####
     void buildBars () {
         closeButton.setParent(&topBar);
         encyclopediaButton.setParent(&topBar);
         radioButton.setParent(&topBar);
         settingsButton.setParent(&topBar);
 
-        encodingButton.setParent(&bottomBar);
-        decodingButton.setParent(&bottomBar);
+        textButton.setParent(&bottomBar);
+        flashButton.setParent(&bottomBar);
     }
 
     void buildLayouts () {
@@ -66,15 +83,18 @@ private:
         topLayout.setAlignment(Qt::AlignTop | Qt::AlignHCenter);
         topBar.setLayout(&topLayout);
 
-        translateLayout.addWidget(&englishTextBox);
-        translateLayout.addWidget(&conversionButton);
-        translateLayout.addWidget(&morseTextBox);
+        translateLayout.addWidget(&inputTextBox);
+        translateLayout.addWidget(&warningLabel);
+        translateLayout.addWidget(&switchBox);
+        translateLayout.addWidget(&outputTextBox);
         translateLayout.setContentsMargins(8, 8, 8, 8);
         translateLayout.setAlignment(Qt::AlignTop | Qt::AlignHCenter);
         translateBox.setLayout(&translateLayout);   
 
-        bottomLayout.addWidget(&encodingButton);
-        bottomLayout.addWidget(&decodingButton);
+        bottomLayout.addWidget(&textButton);
+        bottomLayout.addWidget(&fileButton);
+        bottomLayout.addWidget(&materialButton);
+        bottomLayout.addWidget(&flashButton);
         bottomLayout.setSpacing(0);
         bottomLayout.setContentsMargins(8, 8, 8, 8);
         bottomLayout.setAlignment(Qt::AlignBottom | Qt::AlignHCenter);
@@ -96,8 +116,10 @@ private:
         radioButton.setObjectName("radioButton");
         radioButton.setToolTip("open the broadcast center");
         settingsButton.setObjectName("settingsButton");
-        encodingButton.setObjectName("encodingButton");
-        decodingButton.setObjectName("decodingButton");
+        textButton.setObjectName("textButton");
+        fileButton.setObjectName("fileButton");
+        materialButton.setObjectName("materialButton");
+        flashButton.setObjectName("flashButton");
     }
 
     void buildStyle () {
@@ -105,31 +127,48 @@ private:
         this->setStyleSheet(
         "QWidget {color: #FFFFFF; background-color: #242424;}"
         "QToolTip {background-color: #060606; color: #FFFFFF; border: 1px solid #FFFFFF; border-radius: 0.3em; padding: 0.1em;}"
-        "QTextEdit {font-size: 24px;}"
+        "QTextEdit {font-size: 24px; background-color: #2E2E2E; margin: 0 0.5em; border-radius: 0.71em; padding: 0.5em;}"
         "QPushButton {font-size: 16px; background-color: #2E2E2E; padding: 0.8em; border: none;}"// border-radius: 0.7em;}"
         "QPushButton:hover {background-color: #454545;}"
+
+        "#warningLabel {color: #FD1D58;}"
+
         "#closeButton {border-radius: 0.71em; padding: 0.25em 0.4em;}"
         "#settingsButton, #radioButton, #encyclopediaButton {border-radius: 0.35em; padding: 0.3em 0.5em; background-color: 242424;}"
         "#settingsButton:hover, #radioButton:hover, #encyclopediaButton:hover {background-color: #2D2D2D;}"
-        "#encodingButton {border-top-left-radius: 0.7em; margin-right: 0.2em;}"
-        "#decodingButton {border-top-right-radius: 0.7em; margin-left: 0.2em;}"
         "#settingsButton {margin: 0 0.5em 0 0;}"
         "#encyclopediaButton {margin: 0 0.5em 0 0; font-size: 14px;}"
+        "#swapButton {margin: 0.25em; border-radius: 0.71em; font-size: 32px; padding: 0.2em 0.4em;}"
+
+        "#textButton {border-top-left-radius: 0.7em;}"
+        "#textButton, #fileButton, #materialButton, #flashButton {margin: 0.1em;}"
+        "#fileButton, #materialButton {font-size: 19px; padding: 0.56em}"
+        "#flashButton {border-top-right-radius: 0.7em;}"
         );
     }
 
     void buildTranslateBox () {
-        this->morseTextBox.setReadOnly(true);
-        this->connect(&conversionButton, &QPushButton::clicked, this, &GUI::textToMorse);
+        outputTextBox.setReadOnly(true);
+        swapButton.setObjectName("swapButton");
+        warningLabel.setAlignment(Qt::AlignHCenter);
+        warningLabel.setObjectName("warningLabel");
+        warningLabel.setVisible(false);
+        switchBox.setLayout(&sbLayout);
+        sbLayout.addWidget(&swapButton);
+        sbLayout.setAlignment(Qt::AlignHCenter);
+        sbLayout.setContentsMargins(0, 0, 0, 0);
+        sbLayout.setSpacing(0);
+        connect(&inputTextBox, &QTextEdit::textChanged, this, &GUI::inputChanged);
     }
 
     // #### Button actions:
-    void textToMorse () {
-        const string englishText = this->englishTextBox.toPlainText().toStdString(), letterSep = " ", wordSep = "  ";
+    void inputChanged () {
+        const string englishText = this->inputTextBox.toPlainText().toStdString(), letterSep = " ", wordSep = "  ";
         const char longSignal = '-', shortSignal = '.';
         const QString newQstring = QString::fromStdString(convertText(englishText, shortSignal, longSignal, letterSep, wordSep));
-        this->morseTextBox.setPlainText(newQstring);
-        //this->morseTextBox.setPlainText(QString::fromStdString(convertMorse(englishText)));
+        outputTextBox.setPlainText(newQstring);
+        warningLabel.setVisible(englishText.size() > 300);
+        //this->outputTextBox.setPlainText(QString::fromStdString(convertMorse(englishText)));
     }
 };
 
